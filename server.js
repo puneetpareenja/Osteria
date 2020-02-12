b17b210d414466373c3219e2c9d2f11e1284d738
Puneet Pareenja <pareenjapuneet@gmail.com>
2020-02-11 16:23:12 -0800

    Refactored the folder structure.

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-
-var app = express()
-
-app.use(express.static(__dirname))
-
-var server = app.listen(3000, ()=>{
-    console.log('server is listening on port', server.address().port)
-})
