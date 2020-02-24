fcd00a8ecd5e463255925489ce2300883e274086
Puneet Pareenja <pareenjapuneet@gmail.com>
2020-02-23 20:21:14 -0800

    Created new React App

---
+++ osteria-webapp/src/pages/login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class login extends Component {
+  render() {
+    return <div>Login Page</div>;
+  }
+}
+
+export default login;
---
+++ osteria-webapp/src/pages/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class signup extends Component {
+  render() {
+    return <div>Sign Up Page</div>;
+  }
+}
+
+export default signup;
