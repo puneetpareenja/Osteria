16119d14bca7d2a4f68cf569bda5a2affd4cd0c7
Puneet Pareenja <pareenjapuneet@gmail.com>
2020-03-22 03:38:34 -0700

    Added Ui for the bot

--- osteria-webapp/src/components/Bot.js
+++ osteria-webapp/src/components/Bot.js
@@ -1,25 +1,55 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { sendMessage } from "../redux/chat";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { ChatFeed, Message } from "react-chat-ui";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  paper: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(2),
+    height: "80vh"
+  },
+  textfield: {
+    position: "fixed",
+    // top: 480
+    bottom: 10
+  }
+});
 
 class Bot extends Component {
+  handleKeyDown = event => {
+    const { sendMessage } = this.props;
+    this.setState({ open: true });
+    if (event.keyCode === 13) {
+      sendMessage(event.target.value);
+      event.target.value = "";
+    }
+  };
+
   render() {
-    const { feed, sendMessage } = this.props;
+    const { feed, classes } = this.props;
+    const messageArray = [];
+    console.log(feed);
+    feed.forEach(element => {
+      let message = new Message({ id: element.id, message: element.text });
+      messageArray.push(message);
+    });
+    console.log(messageArray);
     return (
-      <Fragment>
-        <h1>Osteria ChatBot</h1>
-        <ul>
-          {feed.map(entry => (
-            <li>{entry.text}</li>
-          ))}
-        </ul>
-        <input
+      <div>
+        <ChatFeed messages={messageArray} maxHeight={400} />
+
+        <TextField
+          className={classes.textfield}
+          placeholder="Type a message..."
+          fullWidth
           type="text"
-          onKeyDown={e =>
-            e.keyCode === 13 ? sendMessage(e.target.value) : null
-          }
+          onKeyDown={this.handleKeyDown}
         />
-      </Fragment>
+      </div>
     );
   }
 }
@@ -28,4 +58,6 @@ const mapStateToProps = state => ({
   feed: state
 });
 
-export default connect(mapStateToProps, { sendMessage })(Bot);
+export default connect(mapStateToProps, { sendMessage })(
+  withStyles(styles)(Bot)
+);
--- osteria-webapp/src/redux/chat.js
+++ osteria-webapp/src/redux/chat.js
@@ -6,9 +6,9 @@ const client = new ApiAiClient({ accessToken });
 
 const ON_MESSAGE = "ON_MESSAGE";
 
-export const sendMessage = (text, sender = "user") => ({
+export const sendMessage = (text, id = 0) => ({
   type: ON_MESSAGE,
-  payload: { text, sender }
+  payload: { text, id }
 });
 
 const messageMiddleware = () => next => action => {
@@ -22,18 +22,15 @@ const messageMiddleware = () => next => action => {
       const {
         result: { fulfillment }
       } = response;
-      if (fulfillment.speech !== "") {
-        next(sendMessage(fulfillment.speech, "Bot"));
-      } else {
-        fulfillment.messages.forEach(element => {
-          next(sendMessage(element.speech, "Bot"));
-        });
-      }
+
+      fulfillment.messages.forEach(element => {
+        next(sendMessage(element.speech, 1));
+      });
     }
   }
 };
 
-const initState = [{ text: "hey" }];
+const initState = [{ text: "Send a message", id: 1 }];
 const messageReducer = (state = initState, action) => {
   switch (action.type) {
     case ON_MESSAGE:
